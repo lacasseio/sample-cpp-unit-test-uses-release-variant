760f994d22da82af5579f580798af6482a61fd94
Daniel Lacasse <daniel@nokee.dev>
2024-01-26 15:04:29 -0500

    Convert annonymous class back to lambda
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- release-testing-plugin/src/main/java/ReleaseTestingPlugin.java
+++ release-testing-plugin/src/main/java/ReleaseTestingPlugin.java
@@ -54,7 +54,7 @@ public /*final*/ abstract class ReleaseTestingPlugin implements Plugin<Project> 
         extension.getTestedBinarySpec().convention(TestAgainstExtension.TestedBinarySpec.DEBUG);
 
         project.getComponents().withType(CppTestSuite.class).configureEach(testSuite -> {
-            testSuite.getBinaries().whenElementKnown(CppTestExecutable.class, new Action<CppTestExecutable>() {
+            testSuite.getBinaries().whenElementKnown(CppTestExecutable.class, new Action<>() {
                 @Override
                 public void execute(CppTestExecutable testBinary) {
                     //region nativeLink configuration
@@ -84,50 +84,47 @@ public /*final*/ abstract class ReleaseTestingPlugin implements Plugin<Project> 
                     return; // nothing to do
                 }
 
-                testedComponent.getBinaries().whenElementFinalized(new Action<CppBinary>() {
-                    @Override
-                    public void execute(CppBinary testedBinary) {
-                        if (!isTestedBinary(testBinary, testedComponent, testedBinary) || !extension.getTestedBinarySpec().get().isSatisfiedBy(testedBinary)) {
-                            return;
-                        }
+                testedComponent.getBinaries().whenElementFinalized((Action<CppBinary>) testedBinary -> {
+                    if (!isTestedBinary(testBinary, testedComponent, testedBinary) || !extension.getTestedBinarySpec().get().isSatisfiedBy(testedBinary)) {
+                        return;
+                    }
 
-                        // Recreate testable object file collection
-                        final ConfigurableFileCollection testableObjects = project.getObjects().fileCollection();
-                        if (testedComponent instanceof CppApplication) {
-                            // In cases where the task `relocateMainFor*` doesn't exist (for some reason),
-                            //   we can configure the task only when it appears (by name) using `withType(<type>).configureEach(if (<name>) ...).
-                            //   That syntax replace `named(<name>, <type>, ...)`.
-                            //   When wiring the value through `testableObjects` we can use a `Callable` to further defer the task query by name.
-                            project.getTasks().withType(UnexportMainSymbol.class).configureEach(task -> {
-                                if (task.getName().equals("relocateMainFor" + capitalize(qualifyingName(testBinary)))) {
-                                    task.getObjects().setFrom(testedBinary.getObjects());
-                                }
-                            });
-                            testableObjects.from((Callable<?>) () -> {
-                                final TaskProvider<UnexportMainSymbol> unexportMainSymbol = project.getTasks().named("relocateMainFor" + capitalize(qualifyingName(testBinary)), UnexportMainSymbol.class);
-                                return unexportMainSymbol.map(UnexportMainSymbol::getRelocatedObjects);
-                            });
-                        } else {
-                            testableObjects.from(testedBinary.getObjects());
-                        }
-
-                        final Configuration linkConfiguration = project.getConfigurations().getByName("nativeLink" + capitalize(qualifyingName(testBinary)));
-
-                        // Assuming a single FileCollectionDependency which should be the Gradle core object files
-                        //   In cases where this code executes *before* normal Gradle code (for some reason),
-                        //   we can remove the Gradle (previous) testableObjects dependency to replace it with our own.
-                        //   Note that we should normally be able to inspect the dependencies directly via:
-                        //     linkConfiguration.getDependencies().removeIf(it -> it instanceof FileCollectionDependency);
-                        //   Instead we remove any current and future, FileCollectionDependency that doesn't match our testableObjects.
-                        linkConfiguration.getDependencies().all(it -> {
-                            if (it instanceof FileCollectionDependency) {
-                                if (!((FileCollectionDependency) it).getFiles().equals(testableObjects)) {
-                                    linkConfiguration.getDependencies().remove(it);
-                                }
+                    // Recreate testable object file collection
+                    final ConfigurableFileCollection testableObjects = project.getObjects().fileCollection();
+                    if (testedComponent instanceof CppApplication) {
+                        // In cases where the task `relocateMainFor*` doesn't exist (for some reason),
+                        //   we can configure the task only when it appears (by name) using `withType(<type>).configureEach(if (<name>) ...).
+                        //   That syntax replace `named(<name>, <type>, ...)`.
+                        //   When wiring the value through `testableObjects` we can use a `Callable` to further defer the task query by name.
+                        project.getTasks().withType(UnexportMainSymbol.class).configureEach(task -> {
+                            if (task.getName().equals("relocateMainFor" + capitalize(qualifyingName(testBinary)))) {
+                                task.getObjects().setFrom(testedBinary.getObjects());
                             }
                         });
-                        linkConfiguration.getDependencies().add(project.getDependencies().create(testableObjects));
+                        testableObjects.from((Callable<?>) () -> {
+                            final TaskProvider<UnexportMainSymbol> unexportMainSymbol = project.getTasks().named("relocateMainFor" + capitalize(qualifyingName(testBinary)), UnexportMainSymbol.class);
+                            return unexportMainSymbol.map(UnexportMainSymbol::getRelocatedObjects);
+                        });
+                    } else {
+                        testableObjects.from(testedBinary.getObjects());
                     }
+
+                    final Configuration linkConfiguration = project.getConfigurations().getByName("nativeLink" + capitalize(qualifyingName(testBinary)));
+
+                    // Assuming a single FileCollectionDependency which should be the Gradle core object files
+                    //   In cases where this code executes *before* normal Gradle code (for some reason),
+                    //   we can remove the Gradle (previous) testableObjects dependency to replace it with our own.
+                    //   Note that we should normally be able to inspect the dependencies directly via:
+                    //     linkConfiguration.getDependencies().removeIf(it -> it instanceof FileCollectionDependency);
+                    //   Instead we remove any current and future, FileCollectionDependency that doesn't match our testableObjects.
+                    linkConfiguration.getDependencies().all(it -> {
+                        if (it instanceof FileCollectionDependency) {
+                            if (!((FileCollectionDependency) it).getFiles().equals(testableObjects)) {
+                                linkConfiguration.getDependencies().remove(it);
+                            }
+                        }
+                    });
+                    linkConfiguration.getDependencies().add(project.getDependencies().create(testableObjects));
                 });
             });
 
